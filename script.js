d3318dae67c40d24975ed9c8fc61d3bf2d677a98
AditiGi <aditisingh20144@gmail.com>
2023-08-16 21:40:20 +0530

    s

--- script.js
+++ script.js
@@ -1,16 +1,39 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//   const addButton = document.querySelector("#button");
+//   const inputField = document.querySelector("#inputField");
+//   const taskList = document.querySelector("#list");
+
+//   addButton.addEventListener("click", function () {
+//     const newTaskText = inputField.value.trim();
+
+//     if (newTaskText) {
+//       const newTaskItem = document.createElement("li");
+//       newTaskItem.textContent = newTaskText;
+//       taskList.appendChild(newTaskItem);
+//       inputField.value = "";
+//     }
+//   });
+// });
 document.addEventListener("DOMContentLoaded", function () {
-  const addButton = document.querySelector(".button");
+  const addButton = document.querySelector("#button");
   const inputField = document.querySelector("#inputField");
-  const taskList = document.querySelector("#list");
+  const taskList = document.querySelector(".tasks ul");
 
   addButton.addEventListener("click", function () {
     const newTaskText = inputField.value.trim();
 
     if (newTaskText) {
       const newTaskItem = document.createElement("li");
-      newTaskItem.textContent = newTaskText;
+      newTaskItem.innerHTML = `<input type="checkbox">${newTaskText}`;
       taskList.appendChild(newTaskItem);
       inputField.value = "";
+    }
+  });
+
+  taskList.addEventListener("click", function (event) {
+    if (event.target.tagName === "INPUT" && event.target.type === "checkbox") {
+      const listItem = event.target.parentNode;
+      taskList.removeChild(listItem);
     }
   });
 });
